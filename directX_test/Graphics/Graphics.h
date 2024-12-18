cc2a5f06ec23024997771b4b23a14a74c3a4765a
FyX0X <martinergo06@gmail.com>
2024-12-18 21:19:57 +0100

    basic drawing pipeline implemented

--- directX_test/Graphics/Graphics.h
+++ directX_test/Graphics/Graphics.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "AdapterReader.h"
 #include "Shaders.h"
+#include "Vertex.h"
 
 class Graphics
 {
@@ -10,13 +11,15 @@ public:
 private:
 	bool InitializeDirectX(HWND hwnd, int width, int height);
 	bool InitializeShaders();
+	bool InitializeScene();
 
 	Microsoft::WRL::ComPtr<ID3D11Device> device;
 	Microsoft::WRL::ComPtr<ID3D11DeviceContext> deviceContext;
 	Microsoft::WRL::ComPtr<IDXGISwapChain> swapchain;
 	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTargetView;
 
-
 	VertexShader vertexshader;
 	PixelShader pixelshader;
+
+	Microsoft::WRL::ComPtr<ID3D11Buffer> vertexBuffer;
 };
